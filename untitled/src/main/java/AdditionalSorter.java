724f93045f38ae76937660888b56132dd6b51308
Kisurin Konstantin <costya.kisurin@yandex.ru>
2025-09-30 20:21:22 +0300

    feat(additional_sorting): начата реализация дополнительной сортировки

--- untitled/src/main/java/AdditionalSorter.java
+++ untitled/src/main/java/AdditionalSorter.java
@@ -1,27 +1,43 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class AdditionalSorter<T> {
 
-    private T[] innerArray;
     private Comparator<T> comparator;
 
     private AdditionalSorter(Builder builder) {
-        this.innerArray = builder.innerArray;
         this.comparator = builder.comparator;
     }
 
+    public List<T> sort(List<T> arrayForSort) {
+        if (arrayForSort.size() == 1) {
+            return arrayForSort;
+        } else {
+            int midIndex = arrayForSort.size() / 2;
+            List<T> sortedLeftArray = sort(arrayForSort.subList(0, midIndex));
+            List<T> sortedRightArray = sort(arrayForSort.subList(midIndex, arrayForSort.size()));
+
+            return mergeSortedPartsOfArray(sortedLeftArray, sortedRightArray);
+        }
+    }
+
+    private List<T> mergeSortedPartsOfArray(List<T> leftArray, List<T> rightArray) {
+        List<T> resultArray = new ArrayList<>(leftArray.size() + rightArray.size());
+        int indexInLeftArray = 0, indexInRightArray = 0;
+
+        while (indexInLeftArray != leftArray.size() || indexInRightArray != rightArray.size()) {
+
+        }
+
+        return resultArray;
+    }
 
     public class Builder {
 
-        private T[] innerArray;
         private Comparator<T> comparator;
 
         public Builder() {
-        }
-
-        public Builder setInnerArray(T[] innerArray) {
-            this.innerArray = innerArray;
-            return this;
         }
 
         public Builder setComparator(Comparator<T> comparator) {
