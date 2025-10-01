1ab415460b48025cb804684afc8f3ba6eb270c36
Kisurin Konstantin <costya.kisurin@yandex.ru>
2025-10-01 18:05:22 +0300

    feat(additional_sorting): дополнительная сортировка реализована с использованием потоков

--- untitled/src/main/java/AdditionalSorter.java
+++ untitled/src/main/java/AdditionalSorter.java
@@ -1,47 +1,95 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class AdditionalSorter<T> {
 
     private Comparator<T> comparator;
 
-    private AdditionalSorter(Builder builder) {
+    private final ExecutorService executor;
+
+    private AdditionalSorter(Builder<T> builder) {
         this.comparator = builder.comparator;
+        this.executor = builder.executor;
+    }
+
+    public void changeComparatorTo(Comparator<T> comparator) {
+        this.comparator = comparator;
     }
 
     public List<T> sort(List<T> arrayForSort) {
-        if (arrayForSort.size() == 1) {
-            return arrayForSort;
-        } else {
-            int midIndex = arrayForSort.size() / 2;
-            List<T> sortedLeftArray = sort(arrayForSort.subList(0, midIndex));
-            List<T> sortedRightArray = sort(arrayForSort.subList(midIndex, arrayForSort.size()));
+        Future<List<T>> sortResult = executor.submit(() -> {
+            if (arrayForSort.size() == 1 || arrayForSort.isEmpty()) {
+                return arrayForSort;
+            } else {
+                int midIndex = arrayForSort.size() / 2;
+                List<T> sortedLeftArray = sort(arrayForSort.subList(0, midIndex));
+                List<T> sortedRightArray = sort(arrayForSort.subList(midIndex, arrayForSort.size()));
 
-            return mergeSortedPartsOfArray(sortedLeftArray, sortedRightArray);
+                return mergeSortedPartsOfArray(sortedLeftArray, sortedRightArray);
+            }
+        });
+
+        try {
+            return sortResult.get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            System.out.printf("Execution exception: %s\n", e.getMessage());
         }
+
+        return null;
     }
 
     private List<T> mergeSortedPartsOfArray(List<T> leftArray, List<T> rightArray) {
+        if (leftArray == null || rightArray == null) {
+            return null;
+        }
+
         List<T> resultArray = new ArrayList<>(leftArray.size() + rightArray.size());
         int indexInLeftArray = 0, indexInRightArray = 0;
 
         while (indexInLeftArray != leftArray.size() || indexInRightArray != rightArray.size()) {
-
+            if (indexInLeftArray == leftArray.size()) {
+                resultArray.addAll(rightArray.subList(indexInRightArray, rightArray.size()));
+                indexInRightArray = rightArray.size();
+                continue;
+            }
+            if (indexInRightArray == rightArray.size()) {
+                resultArray.addAll(leftArray.subList(indexInLeftArray, leftArray.size()));
+                indexInLeftArray = leftArray.size();
+                continue;
+            }
+            if (comparator.compare(leftArray.get(indexInLeftArray), rightArray.get(indexInRightArray)) >= 0) {
+                resultArray.add(rightArray.get(indexInRightArray));
+                indexInRightArray++;
+            } else {
+                resultArray.add(leftArray.get(indexInLeftArray));
+                indexInLeftArray++;
+            }
         }
 
         return resultArray;
     }
 
-    public class Builder {
+    public static class Builder<T> {
 
         private Comparator<T> comparator;
+        private ExecutorService executor;
 
         public Builder() {
         }
 
-        public Builder setComparator(Comparator<T> comparator) {
+        public Builder<T> setComparator(Comparator<T> comparator) {
             this.comparator = comparator;
+            return this;
+        }
+
+        public Builder<T> setExecutor(ExecutorService executor) {
+            this.executor = executor;
             return this;
         }
 
